a9b9eac65472e12b78cf4a0814abbd30c94a10fc
santhoshkumar1122 <santhosh22.vtalent@gmail.com>
2024-06-28 06:50:08 -0700

    added variables.sh file

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ramesh:: Hello Suresh, Good Morninf"
+echo "Suresh:: Hi Ramesh, very Good Morning"
+echo "Ramesh:: How are you Suresh?"
+echo "Suresh:: I am good Ramesh, How are you"
+
