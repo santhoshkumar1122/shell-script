ab69ce7e4005feef376fdbec9ccdef1c9c8a179e
santhoshkumar1122 <santhosh22.vtalent@gmail.com>
2024-06-29 02:07:27 -0700

    added colours to the succes and failure

--- 12-logs.sh
+++ 12-logs.sh
@@ -2,22 +2,25 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
 then
-   echo "Error: $2 ... failed"
+   echo -e "Error: $2 ... $R failed $N"
    exit 1
 else
-   echo "$2 ... success"
+   echo -e "$2 ... $G success $N"
 fi
 }
 
 if [ $ID -ne 0 ]
 then  
-   echo "Error:: Please run this script with root access"
+   echo -e "$R Error:: Please run this script with root access $N"
    exit 1
 else
    echo "you are a root user"
