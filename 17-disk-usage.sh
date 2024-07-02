0e295cbce855402f1df3ae02146b91f6ffb4ed93
santhoshkumar1122 <santhosh22.vtalent@gmail.com>
2024-07-02 03:41:54 -0700

    committed

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -7,9 +7,9 @@ message=""
 
 while IFS= read line
 do 
-   usage=$(echo $line | awk '{print $6F}' | cut -d % -fi)
+   usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
    partition=$(echo $line | awk '{print $1F}')
-   if [ $usage -gt  $DISK_THRESHOLD ]
+   if [ $usage -ge  $DISK_THRESHOLD ]
    then
        message+="High disk usage on $partition: $usage"
    fi
