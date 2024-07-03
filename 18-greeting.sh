fceeeeebf4baf5c1569fcc230958c9183825f2c4
santhoshkumar1122 <santhosh22.vtalent@gmail.com>
2024-07-03 02:16:51 -0700

    committed

---
+++ 18-greeting.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NAME=""
+WISHES=""
+
+USAGE(){
+    echo "USAGE:: $(basename $0) -n <name> -w <wishes"
+    echo "Options::"
+    echo " -n, Specify the name (mandatory)"
+    echo " -w, Specify the wishes, ex, Good Morning"
+    echo " -h, Display the help and exit"
+}
+
+while getopts ":n:w:h" opt; do
+    case $opt in
+        n) NAME="$OPTORG";;
+        w) WISHES="$OPTARG";;
+        h|* USAGE; exit;;
+    esac
+
+done
