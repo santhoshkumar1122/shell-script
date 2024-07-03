99a4db0bf1c9f02e4677525db36845d6bdce98f5
santhoshkumar1122 <santhosh22.vtalent@gmail.com>
2024-07-03 02:33:37 -0700

    committed

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -15,7 +15,18 @@ while getopts ":n:w:h" opt; do
     case $opt in
         n) NAME="$OPTORG";;
         w) WISHES="$OPTARG";;
-        h|*) USAGE; exit;;
+        \?) echo "invalid options: -"$OPTARG"" >&2; USAGE; exit;;
+        :) USAGE; exit;;
+        h) USAGE; exit ;;
     esac
 
 done
+
+if [ -z "$NAME" ] || [ -z "$WISHES" ]
+     echo "ERROR: both -n and -w are mandatory options."
+     USAGE
+     exit 1
+fi
+
+echo "Hello $NAME. $WISHES. I have been learning shell-script"
+
